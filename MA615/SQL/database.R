a95e2a264d00303b4dc123bc2d8c100aa3e3d1aa
费奕儒 <feiyiru@dhcp-wifi-8021x-168-122-209-133.bu.edu>
2019-11-01 15:44:27 -0400

    hhh

--- MA615/SQL/database.R
+++ MA615/SQL/database.R
@@ -9,6 +9,9 @@ contrib_all = read_excel("Top MA Donors 2016-2020.xlsx",
 Field = read_excel("Top MA Donors 2016-2020.xlsx",
                    sheet = "Field Descriptions")
 
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = ", ",replacement = ",")
+
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "\\s\\w*",replacement = "")
 
 
 contribution = select(contrib_all, cycle, contribid, fam, date,amount, recipid, type, fectransid, cmteid) %>% distinct()
@@ -17,7 +20,7 @@ recipient = select(contrib_all, recipid,party, recipient,recipcode) %>% distinct
 organization = select(contrib_all, orgname, ultorg) %>% distinct() %>% na.omit()
 
 
-Con = dbConnect(SQLite(),"Yiru.sqlite")
+Con = dbConnect(SQLite(),"Yiru_Fei.sqlite")
 dbWriteTable(Con,"Contribution",contribution)
 dbWriteTable(Con,"Contributor",contributor)
 dbWriteTable(Con,"Recipient",recipient)
